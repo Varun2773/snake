4aaaf6789d79883954048cba99fd48a69b3cc9bf
Varun2773 <varunmathiyalagan@gmail.com>
2024-02-18 22:55:58 +0530

    13-03-2024 22:55 today work is over time to sleep

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // define html element
-con
+const board = document.getElementById('game-board');
+
+//definr game variables
+let snake = [{x: 10,y: 10}];
+
+//draw game map, snake food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+//draw snake
+function  drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div','snake');
+
+        // last a complete at 33.38 min
+
+    })
+}
